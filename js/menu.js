01a330d19711d66640bcaf43ea4dd1beecc1bb78
MayraSalg <mayra.salgaraeva@gmail.com>
2022-06-06 08:47:37 +0300

    //

--- js/menu.js
+++ js/menu.js
@@ -5,8 +5,11 @@ let replyOpen = document.querySelector(".action-buttons__message");
 let replyClose = document.querySelector(".close-button-img");
 let reply = document.querySelector(".reply");
 let callBackOpen = document.querySelector(".action-buttons__tel");
-let callBackText = document.querySelector(".callback-text");
-let replyText = document.querySelector(".reply-text");
+let callBackText = document.querySelector(".reply-text");
+let callBackMess = document.getElementById("mess");
+let callBackName = document.getElementById("name");
+let callBackEmail = document.getElementById("email");
+let callBackFooter = document.querySelector(".reply-footer");
 popUpToggle.onclick = function (){
     popUp.style.display="flex";
     popUp.style.width="320px"; //открыть главное меню
@@ -23,18 +26,26 @@ popUpClose.onclick = function (){
 replyOpen.onclick = function (){
     reply.style.display="inline";
     reply.style.position="absolute";
-    popUp.style.display="none";//открыть reply
+    popUp.style.display="none";       //открыть reply
     popUp.style.width="0px";
     popUp.style.height="0px";
+    callBackText.textContent="Обратная связь";
+    callBackMess.classList.add(".reply-body__form");
+    callBackFooter.style.top="0px";
 }
 replyClose.onclick = function (){
-    reply.style.display="none"; //закрыть reply
+    reply.style.display="none";         //закрыть reply
 }
 callBackOpen.onclick = function (){
     reply.style.display="inline";
-    reply.style.position="absolute"; //открыть обратный звонок
+    reply.style.position="absolute";   //открыть обратный звонок
     popUp.style.display="none";
     popUp.style.width="0px";
     popUp.style.height="0px";
+    callBackText.textContent="Томас-сосиска";
+    callBackMess.remove(".reply-body__form");
+    callBackName.remove(".reply-body__form");
+    callBackEmail.remove(".reply-body__form");
+    callBackFooter.style.top="-270px";
 
 }
