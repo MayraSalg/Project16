b465415d40d6a5873544f0367f862481d2714575
MayraSalg <mayra.salgaraeva@gmail.com>
2022-06-09 13:53:57 +0300

    ///

--- js/menu.js
+++ js/menu.js
@@ -5,16 +5,18 @@ let replyOpen = document.querySelector(".action-buttons__message");
 let replyClose = document.querySelector(".close-button-img");
 let reply = document.querySelector(".reply");
 let callBackOpen = document.querySelector(".action-buttons__tel");
-let callBackText = document.querySelector(".reply-text");
-let callBackMess = document.getElementById("mess");
-let callBackName = document.getElementById("name");
-let callBackEmail = document.getElementById("email");
-let callBackFooter = document.querySelector(".reply-footer");
+let callBack = document.querySelector(".call-back");
+let blur = document.querySelector(".line");
+let callBackClose = document.querySelector(".close-button__call-back");
+let telephone = document.querySelector(".telephone-button");
+let message = document.querySelector(".message-button");
 
 popUpToggle.onclick = function (){
+
     popUp.style.display="flex";
     popUp.style.width="320px"; //открыть главное меню
-    popUp.style.height="1024px";
+    popUp.style.height="1524px";
+    blur.style.display="inline";
 
 };
 
@@ -22,7 +24,13 @@ popUpClose.onclick = function (){
     popUp.style.display="none";
     popUp.style.width="0px";//закрыть главное меню
     popUp.style.height="0px";
+    blur.style.display="none";
 
+}
+window.onclick = function (event){
+    if (event.target == popUp){
+        popUp.style.display="none";
+    }
 }
 replyOpen.onclick = function (){
     reply.style.display="inline";
@@ -31,24 +39,45 @@ replyOpen.onclick = function (){
     popUp.style.width="0px";
     popUp.style.height="0px";
     callBackText.textContent="Обратная связь";
-
     callBackFooter.style.top="0px";
 
 
 }
 replyClose.onclick = function (){
-    reply.style.display="none";         //закрыть reply
+    reply.style.display="none";
+    blur.style.display="none";
+    callBack.style.display="none";//закрыть reply
 }
 callBackOpen.onclick = function (){
-    reply.style.display="inline";
-    reply.style.position="absolute";   //открыть обратный звонок
+    callBack.style.display="inline-block";
+    //открыть обратный звонок
     popUp.style.display="none";
     popUp.style.width="0px";
     popUp.style.height="0px";
-    callBackText.textContent="Томас-сосиска";
-    callBackMess.remove(".reply-body__form");
-    callBackName.remove(".reply-body__form");
-    callBackEmail.remove(".reply-body__form");
     callBackFooter.style.top="-270px";
+    callBack.style.position="absolute";
+}
+callBackClose.onclick = function (){
+    callBack.style.display="none";
+    blur.style.display="none";
 
+}
+telephone.onclick= function (){
+    callBack.style.display="inline-block";
+    //открыть обратный звонок
+    popUp.style.display="none";
+    popUp.style.width="0px";
+    popUp.style.height="0px";
+    callBackFooter.style.top="-270px";
+    callBack.style.position="absolute";
+    blur.style.display="inline";
+}
+message.onclick= function (){
+    reply.style.display="inline";
+    reply.style.position="absolute";
+    popUp.style.display="none";       //открыть reply
+    popUp.style.width="0px";
+    popUp.style.height="0px";
+    callBackText.textContent="Обратная связь";
+    callBackFooter.style.top="0px";
 }
